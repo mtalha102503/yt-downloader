54ee68150c9e1ade10004eed39abb5501db3fb18
Muhammad Talha <realonlinejobs56@gmail.com>
2025-06-08 06:47:35 -0700

    Use process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -247,8 +247,7 @@ app.get('/privacy', (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
