2146bb108c33d92352261a8f0ed87d6a647bb83b
Muhammad Talha <realonlinejobs56@gmail.com>
2025-06-08 06:51:27 -0700

    Fix: Use process.env.PORT for Render

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
+# Create bin directory
 mkdir -p ./bin
+
+# Download yt-dlp binary
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./bin/yt-dlp
+
+# Make it executable
 chmod +x ./bin/yt-dlp
 
