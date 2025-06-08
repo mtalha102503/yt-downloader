c5f989c5d85c65f5df594999d8ccb64de8b527c1
mtalha102503 <realonlinejobs56@gmail.com>
2025-06-08 06:30:58 -0700

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y curl
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
